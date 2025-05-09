cf88fff0c0b6914973f1b2439af720ca1719fdce
Agus <agustinferres4403@gmail.com>
2025-05-09 17:48:00 -0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       link: [
-        { rel: 'icon', type: 'image/png', href: 'https://avatars.githubusercontent.com/u/211045522?s=400&u=3d05478ffdba3ce1178d34d7026b57974ea95186&v=4' },
+        { rel: 'icon', type: 'image/png', href: '/mnq.png' },
       ]
     }
   }
