8203fcb38f3b7f68d65e16e021215845c7f2f833
Agus <agustinferres4403@gmail.com>
2025-05-10 18:48:08 -0300

    added background

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  css: [ '~/assets/css/main.css' ],
   app: {
     head: {
       title: 'MnQ', // default fallback title
@@ -12,7 +33,7 @@ export default defineNuxtConfig({
       },
       link: [
         { rel: 'icon', type: 'image/png', href: '/mnq.png' },
-      ]
-    }
-  }
-})
+      ],
+    },
+  },
+});
